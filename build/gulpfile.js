3a0337ef6812be4ac1ebfea1e8d7f1522e4f872e
mantou <676015863@qq.com>
2019-09-11 19:18:56 +0800

    去掉预览页面用不了的代码

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -2,6 +2,14 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const packageJson = require('../package.json');
 
+// 预览页面用的
+const jsPreviewAssetsMin = [
+  // React
+  './node_modules/react/umd/react.production.min.js',
+  './node_modules/react-dom/umd/react-dom.production.min.js',
+  // PubSub
+  './node_modules/PubSub/dist/pubsub.min.js'
+];
 
 const jsAssetsMin = [
   // React
@@ -33,6 +41,13 @@ const jsAssets = [
   './node_modules/PubSub/src/pubsub.js'
 ];
 
+gulp.task('build-preview-js:min', () => {
+  return gulp
+    .src(jsPreviewAssetsMin)
+    .pipe(concat(`h5ds-vendor-preview-${packageJson.version}.min.js`, { newLine: '\n\n' }))
+    .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('build-js:min', () => {
   return gulp
     .src(jsAssetsMin)
@@ -47,4 +62,4 @@ gulp.task('build-js', () => {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', gulp.parallel('build-js', 'build-js:min'));
+gulp.task('default', gulp.parallel('build-js', 'build-js:min', 'build-preview-js:min'));
